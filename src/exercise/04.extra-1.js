6176db264ff0d69b123b11865935e4864a2f696f
Kate <test@gmail.com>
2021-01-22 07:38:47 -0500

    getTogglerProps

---
+++ src/exercise/04.extra-1.js
@@ -0,0 +1,45 @@
+// Prop Collections and Getters
+// http://localhost:3000/isolated/exercise/04.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function callAll(...fns) {
+  return args => fns.forEach(fn => fn?.(args))
+}
+
+function useToggle() {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  function getTogglerProps({onClick, ...props}) {
+    return {
+      'arial-pressed': on + '',
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+  return {on, toggle, getTogglerProps}
+}
+
+function App() {
+  const {on, getTogglerProps} = useToggle()
+  return (
+    <div>
+      <Switch on={on} {...getTogglerProps({})} />
+      <hr />
+      <button
+        aria-label="custom-button"
+        {...getTogglerProps({
+          'arial-label': 'custom-button',
+          onClick: () => console.log('yo'),
+          id: 'custom-button-id',
+        })}
+      >
+        {on ? 'on' : 'off'}
+      </button>
+    </div>
+  )
+}
+
+export default App
